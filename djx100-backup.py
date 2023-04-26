cfc1cc2d238a09c4123f97e29abc4064beb26541
musen23872 <musen23872@gmail.com>
2023-04-26 20:36:08 +0900

    initial commit

---
+++ djx100-backup.py
@@ -0,0 +1,103 @@
+# MIT License
+#
+# Copyright (c) 2023 musen23872
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+import serial
+import argparse
+import os
+
+def main():
+    parser = argparse.ArgumentParser(description='Alinco DJ-X100 **Unofficial** Memory Backup Script')
+    parser.add_argument('-o', '--output', type=str, required=True, help='Output file name (required)')
+    parser.add_argument('-p', '--port', type=str, required=True, help='Serial port name (required)')
+    parser.add_argument('-f', '--force', action='store_true', help='Overwrite output file without confirmation')
+    parser.add_argument('-b', '--baudrate', type=int, default=115200, help='Baud rate (default: 115200)')
+    args = parser.parse_args()
+
+    header_magic_number = 'X100'
+    header_version = 1
+    header_data_size = 128000
+    header_comment = 'Alinco DJ-X100 **Unofficial** Memory Backup Data'
+
+    start_addr = 0x20000
+    end_addr = 0x3f3ff
+
+    try:
+        ser = serial.Serial(args.port, args.baudrate, timeout=5, write_timeout=5)
+    except serial.serialutil.SerialException as e:
+        print(f"Error: {e}. Could not open port '{args.port}'. Please check the port name and try again.")
+        exit(-1)
+
+    cmd = 'AL~DJ-X100\r'
+    try:
+        ser.write(cmd.encode())
+    except serial.SerialTimeoutException:
+        print("Error: Write operation timed out. Please check the connection and try again.")
+        exit(-1)
+
+    ser.readline()
+    response = ser.readline().strip().decode()
+    if response != 'OK':
+        print("Error: No response from the device or timed out. Please check the connection and try again.")
+        exit(-1)
+
+    page_total = int((end_addr - start_addr) / 0x100) + 1
+
+    if not args.force and os.path.exists(args.output):
+        confirm = input(f"The file '{args.output}' already exists. Do you want to overwrite it? (y/N): ")
+        if confirm.lower() != 'y':
+            print("Aborted.")
+            exit(0)
+
+    try:
+        with open(args.output, 'wb') as f:
+            header = \
+                (header_magic_number + '\0' * 4)[:4].encode() + \
+                header_version.to_bytes(1) + \
+                header_data_size.to_bytes(4, 'little') + \
+                (header_comment + '\0' * 64)[:64].encode()
+            header += ('\0' * 256).encode()
+            f.write(header[:256])
+
+            for current_page, addr in enumerate(range(start_addr, end_addr, 0x100), start=1):
+                cmd = f'AL~F{addr:05X}M\r'
+                print(f'\rReading memory page {current_page} of {page_total}', end='', flush=True)
+                try:
+                    ser.write(cmd.encode())
+                except serial.SerialTimeoutException:
+                    print("\nError: Write operation timed out. Please check the connection and try again.")
+                    exit(-1)
+
+                ser.readline()
+                response = ser.readline().strip().decode()
+                if not response:
+                    print("\nError: No response from the device or timed out. Please check the connection and try again.")
+                    exit(-1)
+                binary_data = bytes.fromhex(response)
+                f.write(binary_data)
+
+            print("\nMemory backup completed successfully. Saved to:", args.output)
+    except PermissionError as e:
+        print(f"Error: {e}. Could not open the file '{args.output}' for writing. Please check the file permissions.")
+
+if __name__ == '__main__':
+    main()
+
---
+++ djx100-restore.py
@@ -0,0 +1,125 @@
+# MIT License
+#
+# Copyright (c) 2023 musen23872
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+import serial
+import argparse
+import os
+
+HEADER_MAGIC_NUMBER = b'X100'
+HEADER_VERSION = 1
+HEADER_DATA_SIZE = 128000
+HEADER_LENGTH = 256
+
+def main():
+    parser = argparse.ArgumentParser(description='Alinco DJ-X100 memory restore script')
+    parser.add_argument('-i', '--input', type=str, required=True, help='Input file name (required)')
+    parser.add_argument('-p', '--port', type=str, required=True, help='Serial port name (required)')
+    parser.add_argument('-b', '--baudrate', type=int, default=115200, help='Baud rate (default: 115200)')
+    args = parser.parse_args()
+
+    start_addr = 0x20000
+    end_addr = 0x3f3ff
+
+    if not os.path.exists(args.input):
+        print(f"Error: The file '{args.input}' does not exist. Please check the file name and try again.")
+        exit(0)
+
+    try:
+        with open(args.input, 'rb') as f:
+            magic_number = f.read(4)
+            version = int.from_bytes(f.read(1), byteorder='little')
+            data_size = int.from_bytes(f.read(4), byteorder='little')
+            
+            if magic_number != HEADER_MAGIC_NUMBER:
+                print("Error: Invalid magic number. Please check the input file.")
+                exit(-1)
+            if version != HEADER_VERSION:
+                print("Error: Unsupported file version. Please check the input file.")
+                exit(-1)
+
+            f.seek(0, os.SEEK_END)
+            actual_data_size = f.tell() - HEADER_LENGTH
+            if data_size != actual_data_size:
+                print("Error: Invalid data size. Please check the input file.")
+                exit(-1)
+
+            f.seek(HEADER_LENGTH, os.SEEK_SET)
+            data = f.read(data_size)
+    except PermissionError as e:
+        print(f"Error: {e}. Could not open the file '{args.input}' for reading. Please check the file permissions.")
+        exit(-1)
+
+    try:
+        ser = serial.Serial(args.port, args.baudrate, timeout=5, write_timeout=5)
+    except serial.serialutil.SerialException as e:
+        print(f"Error: {e}. Could not open port '{args.port}'. Please check the port name and try again.")
+        exit(-1)
+
+    cmd = 'AL~DJ-X100\r'
+    try:
+        ser.write(cmd.encode())
+    except serial.SerialTimeoutException:
+        print("Error: Write operation timed out. Please check the connection and try again.")
+        exit(-1)
+
+    ser.readline()
+    response = ser.readline().strip().decode()
+    if response != 'OK':
+        print("Error: No response from the device or timed out. Please check the connection and try again.")
+        exit(-1)
+
+    page_total = int((end_addr - start_addr) / 0x100) + 1
+    current_page = 0
+
+    for addr in range(start_addr, end_addr + 1, 0x100):
+        current_page += 1
+        offset = addr - start_addr
+
+        # Read current data from the device
+        cmd = f'AL~F{addr:05X}M\r'
+        ser.write(cmd.encode())
+        ser.readline()
+        current_data = bytes.fromhex(ser.readline().strip().decode())
+
+        # Compare and write new data if it's different
+        for i in range(0, 256, 128):
+            new_data = data[offset + i:offset + i + 128]
+            if new_data != current_data[i:i + 128]:
+                cmd = f'AL~F{addr + i:05X}W{new_data.hex().upper()}\r'
+                ser.write(cmd.encode())
+                ser.readline()
+                response = ser.readline().strip().decode()
+                if response != 'OK':
+                    print(f"Error: Failed to write data at address {addr + i:05X}. Please check the connection and try again.")
+                    exit(-1)
+
+        print(f"Restoring memory: {current_page}/{page_total}", end='\r')
+
+    # Restart the device
+    cmd = 'AL~RESTART\r'
+    ser.write(cmd.encode())
+    print("\nMemory restore complete. Device has been restarted.")
+
+    ser.close()
+
+if __name__ == '__main__':
+    main()
